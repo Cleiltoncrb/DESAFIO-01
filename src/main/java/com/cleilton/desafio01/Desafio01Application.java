dc5b467317ed50d735670d314eaecd48d41153f1
Cleilton-CCABR <cleiltoncrb@fab.mil.br>
2024-12-16 16:29:04 -0300

    Ajuste, para aplicação ficar interativa com o usuário, utilizando Scanner

--- src/main/java/com/cleilton/desafio01/Desafio01Application.java
+++ src/main/java/com/cleilton/desafio01/Desafio01Application.java
@@ -4,6 +4,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 @SpringBootApplication
 public class Desafio01Application implements CommandLineRunner {
 
@@ -19,16 +22,47 @@ public class Desafio01Application implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        Pedido pedido1 = new Pedido("154",50.00, 10.00);
-        double total1 = ordemService.calcularTotal(pedido1);
-        System.out.printf(" - Total do Pedido: R$ %.2f%n%n", total1);
 
-        Pedido pedido2 = new Pedido("125",150.00, 20.00);
-        double total2 = ordemService.calcularTotal(pedido2);
-        System.out.printf(" - Total do Pedido: R$ %.2f%n%n", total2);
+        Scanner scanner = new Scanner(System.in);
 
-        Pedido pedido3 = new Pedido("2345",250.00, 15.00);
-        double total3 = ordemService.calcularTotal(pedido3);
-        System.out.printf(" - Total do Pedido: R$ %.2f%n%n", total3);
+        boolean continuar = true;
+
+        System.out.print("\n\n");
+
+        while (continuar) {
+
+            System.out.print("Digite o valor básico do pedido: R$ ");
+            double valorBasico = scanner.nextDouble();
+
+            System.out.print("Digite o desconto do pedido (em %): ");
+            double desconto = scanner.nextDouble();
+
+            // Cria o pedido com os dados fornecidos
+            Pedido pedido = new Pedido(valorBasico, desconto);
+            double total = ordemService.calcularTotal(pedido);
+            System.out.printf(" - Total do Pedido: R$ %.2f%n%n", total);
+
+
+            String resposta;
+            boolean respostaValida = false;
+            while (!respostaValida) {
+                System.out.print("\n Deseja fazer outro pedido? \n Digite ( 1 para Sim ) ou ( 0 para Não ) ");
+                resposta = scanner.next();
+
+                if (resposta.equals("1")) {
+                    respostaValida = true;  // Continuar para o próximo pedido
+                } else if (resposta.equals("0")) {
+                    respostaValida = true;  // Finalizar o loop
+                    continuar = false;  // Sai do loop
+                } else {
+                    System.out.println("\n\n Por favor, digite 1 para Sim ou 0 para Não.");
+                }
+            }
+        }
+
+        System.out.println("\n\n\n Obrigado por usar o sistema de pedidos!");
+        scanner.close();
     }
+
+
 }
